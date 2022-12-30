1593c38cd7ea61a2b19ec1ddff591cda1750629b
Petit Prince <arthur.reppelin@gmail.com>
2022-12-30 13:19:02 +0100

    Update spinner.py

--- libs/spinner.py
+++ libs/spinner.py
@@ -17,12 +17,12 @@ from itertools import cycle
 
 
 class spinner:
-    def __init__(self, desc='Processing...', end='Done', timeout=0.1):
+    def __init__(self, desc='Processing...', timeout=0.1, old=False):
         self.desc = desc
-        self.end = end
         self.timeout = timeout
         self._thread = Thread(target=self._animate, daemon=True)
         self.steps = ["[⢿]", "[⣻]", "[⣽]", "[⣾]", "[⣷]", "[⣯]", "[⣟]", "[⡿]"]
+        if old: self.steps = ["[-]", "[\]", "[|]", "[/]"]
         self.done = False
 
     def start(self):
@@ -46,35 +46,6 @@ class spinner:
     def __exit__(self, exc_type, exc_value, tb):
         self.stop()
 
-class old_spinner:
-    def __init__(self, desc='Processing...', end='Done', timeout=0.1):
-        self.desc = desc
-        self.end = end
-        self.timeout = timeout
-        self._thread = Thread(target=self._animate, daemon=True)
-        self.steps = ["[-]", "[\]", "[|]", "[/]"]
-        self.done = False
-
-    def start(self):
-        self._thread.start()
-        return self
-
-    def _animate(self):
-        for c in cycle(self.steps):
-            if self.done:
-                break
-            print(f"\r{c}  {self.desc}", flush=True, end="\r")
-            sleep(self.timeout)
-
-    def __enter__(self):
-        self.start()
-
-    def stop(self):
-        print(' '*(len(self.desc) + 5), end="\r")
-        self.done = True
-
-    def __exit__(self, exc_type, exc_value, tb):
-        self.stop()
         
 ################################################################################
 
@@ -83,6 +54,6 @@ if __name__ == "__main__":
     with spinner("Spinning demo..."):
         for i in range(10):
             sleep(0.25)
-    with old_spinner("Spinning demo..."):
+    with spinner("Spinning demo...", 0.1, True):
         for i in range(10):
             sleep(0.25)
